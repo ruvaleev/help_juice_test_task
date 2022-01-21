cc9664d14db2f86885e40c7a9ca310b27807284d
Ruslan Valeev <ruvaleev@gmail.com>
2022-01-22 00:40:22 +0700

    Fix importmaps

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,5 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin_all_from 'app/javascript/functions', under: 'functions'
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.1/lib/assets/compiled/rails-ujs.js"
